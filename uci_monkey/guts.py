d2d45018bed577bde6a64b12df98d8b04156522b
Karel Kočí <karel.koci@nic.cz>
2019-06-17 14:25:08 +0200

    fixup! WIP

--- uci_monkey/guts.py
+++ uci_monkey/guts.py
@@ -1,97 +1,67 @@
 """These are abstraction for building and managing uci_monkey internal
 configuration representation.
 """
-import string
-import random
+import re
 import collections
+import threading
+from copy import copy
 from . import uci
 
-_ANONYM_CONF_LEN = 6
-_anonym_conf_name_v = set()
 
-
-def _anonym_conf_name():
-    """Generates new name for anonymous sections.
-    """
-    global _anonymous_conf_name_v
-    value = ''.join(random.choices(string.ascii_lowercase + string.digits, k=_ANONYM_CONF_LEN))
-    if value in _anonym_conf_name_v:
-        return _anonym_conf_name()
-    _anonym_conf_name_v.add(value)
-    return "cfg" + value
-
-
-class UciState(dict):
-    """Top level configuration state. It allows you to overly and in general
+class UciDir(dict):
+    """UCI configuration state. It allows you to overly and in general
     manage current UCI configuration. Just instanciate it to create new state
-    overlay. To drop it you call wipe().
+    overlay. To drop it you call drop().
     Note that states automatically create linked-list chain.
     """
 
-    def __init__(self, extend, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        self.lock = threading.Lock()
+        self._re_option = re.compile(r'\w+')
+        self._re_section = re.compile(r'\w+|@\w+\[\d+\]')
         super().__init__(*args, **kwargs)
-        self.extends = extend
-        self.parent = uci._state
-        uci._state = self
-
-    def __del__(self):
-        self.drop()
 
     def __setitem__(self, key, value):
-        if not isinstance(value, UciConfig):
-            raise TypeError("Only instances of UciConfig can be assigned")
-        super().__setitem__(key, value)
+        pkey = key.split('.')
+        if not 2 <= len(pkey) <= 3:
+            raise InvalidUciSpecException("Only allowed keys are of form: config.section[.option]")
+        if not self._re_option.fullmatch(pkey[0]):
+            raise InvalidUciSpecException("Section 'config' does not conform to appropriate format")
+        if not self._re_section.fullmatch(pkey[1]):
+            raise InvalidUciSpecException("Section 'section' does not conform to appropriate format")
+        if len(pkey) == 3 and not self._re_option.fullmatch(pkey[2]):
+            raise InvalidUciSpecException("Section 'option' does not conform to appropriate format")
+        if len(pkey) == 3 and isinstance(value, collections.Iterable) and not isinstance(value, str):
+            super().__setitem__(key, list(value))
+        else:
+            super().__setitem__(key, str(value))
+
+
+class UciState(dict):
+    """UCI configuration state. It allows you to overly and in general
+    manage current UCI configuration. Just instanciate it to create new state
+    overlay. To drop it you call drop().
+    Note that states automatically create linked-list chain.
+    """
+
+    def __setitem__(self, path, value):
+        if not isinstance(value, UciDir):
+            raise TypeError("'{}' is not of UciDir instance".format(type(value)))
+        super().__setitem__(path, list(value))
 
     def __missing__(self, key):
-        if self.extends:
-            return self.parent[key]
-        raise KeyError(key)
+        self[key] = UciDir()
+        return self[key]
 
-    def drop(self):
-        """Drop this state from Uci. This causes also removal of all states
-        instanciated created after this one (child states).
-        """
-        # TODO free all up to this state from current one
-        uci._state = self.parent
+    def conf(self):
+        """Returns default directory for confdir."""
+        return self["/etc/config"]
+
+    def save(self):
+        """Returns default directory for savedir."""
+        return self["/tmp/.uci"]
 
 
-class UciConfig(collections.OrderedDict):
-    """Container for Uci sections. It replaces UCI configuration file.
+class InvalidUciSpecException(Exception):
+    """Provided UCI config.section.option specifier is invalid.
     """
-
-    def __init__(self, *args, **kwargs):
-        rargs = list(args)
-        for arg in rargs:
-            if isinstance(arg, UciSection):  # This is anonymous section
-                kwargs[_anonym_conf_name()] = arg
-                rargs.remove(arg)
-        super().__init__(*rargs, **kwargs)
-
-    def __setitem__(self, key, value):
-        if not isinstance(value, UciSection):
-            raise TypeError("Only instances of UciConfig can be assigned")
-        super().__setitem__(key, value)
-
-    def add(self, section):
-        """Add anonymous section.
-        """
-        self[_anonym_conf_name()] = section
-
-
-class UciSection(dict):
-    """Container for lists and options. Section can also have name (value).
-    """
-
-    def __init__(self, type, *args, **kwargs):
-        self._type = type
-        super().__init__(*args, **kwargs)
-
-    def __setitem__(self, key, value):
-        if not isinstance(value, collections.Iterable):
-            raise TypeError("Only strings and interables can be assigned")
-        super().__setitem__(key, value)
-
-    def type(self):
-        """Return type of this section.
-        """
-        return self.type
